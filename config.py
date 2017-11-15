182cbb4d97e6431b1ea4d573d804070afc8729b1
Klaas Stuhrenberg <klaasstue@aim.com>
2017-11-15 14:10:16 +0100

    Etwas aufgeräumt

--- config.py
+++ config.py
@@ -10,8 +10,6 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-#    SQLALCHEMY_DATABASE_URI = 'postgres://snejfynfkamndr:e1e52fad2b620327b2c96486dae0fbff21ed90f4868b45e0642bb58a87b1624b@ec2-107-20-195-181.compute-1.amazonaws.com:5432/d274hkqapbq4ra'
-#    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'metadata.db')
 
 class ProductionConfig(Config):
     DEBUG = False
@@ -20,8 +18,6 @@ class ProductionConfig(Config):
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-    BIBLIOTHEK = os.environ['BIBLIOTHEK']
-    CALIBREDIR = "%s/.calibre" % os.environ['HOME']
 
 
 class TestingConfig(Config):
